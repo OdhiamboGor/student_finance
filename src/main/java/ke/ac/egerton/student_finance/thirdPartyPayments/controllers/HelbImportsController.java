9367d07a78891e3b689eaa8e4c9a9f63a48b980c
EPeterGor <pgor@egerton.ac.ke>
2020-03-04 15:16:46 +0300

    Created Helb Import Controller

---
+++ src/main/java/ke/ac/egerton/student_finance/thirdPartyPayments/controllers/HelbImportsController.java
@@ -0,0 +1,12 @@
+package ke.ac.egerton.student_finance.thirdPartyPayments.controllers;
+
+import ke.ac.egerton.student_finance.thirdPartyPayments.services.HelbImportsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelbImportsController {
+
+    @Autowired
+    private HelbImportsService helbImportsService;
+}
