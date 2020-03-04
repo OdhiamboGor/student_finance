ba91920c54128b4a1f1466245254bee767b72978
EPeterGor <pgor@egerton.ac.ke>
2020-03-04 15:25:11 +0300

    Created import helb Service

---
+++ src/main/java/ke/ac/egerton/student_finance/thirdPartyPayments/services/HelbImportsService.java
@@ -0,0 +1,11 @@
+package ke.ac.egerton.student_finance.thirdPartyPayments.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface HelbImportsService {
+
+
+
+}
