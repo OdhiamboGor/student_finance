65d3bc5fa89fd30aadd624dd2231610245f6bbff
EPeterGor <pgor@egerton.ac.ke>
2020-03-05 15:09:13 +0300

    removed checking if Helb already exist code

--- src/main/java/ke/ac/egerton/student_finance/thirdPartyPayments/services/HelbImportsService.java
+++ src/main/java/ke/ac/egerton/student_finance/thirdPartyPayments/services/HelbImportsService.java
@@ -2,10 +2,10 @@ package ke.ac.egerton.student_finance.thirdPartyPayments.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public interface HelbImportsService {
 
-
-
+    boolean uploadHelb(MultipartFile file);
 }
